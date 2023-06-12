753a006af9322edd79934b6f5f90d8ee94153c22
2chaelim <charry8540@gmail.com>
2023-06-12 18:01:49 +0900

    0612

--- common.js
+++ common.js
@@ -1,27 +1,36 @@
+const main_bg = document.querySelector(".main_bg");
+const hd = document.querySelector(".hd");
+const main = document.querySelector(".main");
+const main_typo = document.querySelector(".main_typo");
 
-    const main_bg = document.querySelector('.main_bg')
-    const hd = document.querySelector('.hd')
-    const main = document.querySelector('.main')
-    const main_typo = document.querySelector('.main_typo')
+setTimeout(() => {
+  main_bg.classList.add("hidden");
+}, 4000);
+setTimeout(() => {
+  main.classList.add("on");
+}, 5000);
+setTimeout(() => {
+  hd.classList.add("on");
+}, 4500);
 
-    setTimeout(() => {
-        main_bg.classList.add('hidden')
-    }, 4000);
-    setTimeout(() => {
-       main.classList.add('on')
-    }, 5000);
-    setTimeout(() => {
-        hd.classList.add('on')
-     }, 4500);
+let color_picker = document.getElementById("color_picker");
+function changeTextColor() {
+  const text_color1 = document.getElementById("text_color1");
+  const text_color2 = document.getElementById("text_color2");
+  const box_border = document.querySelector(".color_box li:nth-child(4)");
 
-     function changeTextColor(){
-        const color_picker=document.getElementById('color_picker')
-        const text_color1=document.getElementById('text_color1')
-        const text_color2=document.getElementById('text_color2')
-        const box_border=document.querySelector('.color_box li:nth-child(4)')
+  text_color1.style.color = color_picker.value;
+  text_color2.style.color = color_picker.value;
+  box_border.style.backgroundColor = color_picker.value;
+}
+color_picker.addEventListener("input", changeTextColor);
 
-        text_color1.style.color = color_picker.value
-        text_color2.style.color = color_picker.value
-        box_border.style.backgroundColor=color_picker.value
-     }
-     
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
